ce2be224301e8555d3e313d099ae9007604709e2
ArianeThe <ariane.thebault2022@campus-eni.fr>
2024-12-17 12:53:16 +0100

    initial commit

---
+++ src/main/java/com/movies/view/controllers/moviesController.java
@@ -0,0 +1,5 @@
+package com.movies.view.controllers;
+
+public class moviesController {
+
+}
---
+++ src/test/java/com/movies/view/ViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
